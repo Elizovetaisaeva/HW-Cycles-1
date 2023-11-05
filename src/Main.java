e7125a0ae54e9cf3ab2fbd244402cfe5c0a1fd5b
Лаисат <lz_256@mail.ru>
2023-11-05 16:12:49 +0300

    hw cycles updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,91 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+            task1();
+            task2();
+            task3();
+            task4();
+            task5();
+            task6();
+            task7();
+            task8();
+            task9();
+            task10();
+
+        }
+
+        public static void task1() {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+        }
+
+        public static void task2() {
+            for (int i = 10; i >= 1; i--) {
+                System.out.println(i);
+            }
+        }
+
+        public static void task3() {
+            for (int i = 0; i < 17; i = i + 2) {
+                System.out.println(i);
+            }
+
+        }
+
+        public static void task4() {
+            for (int i = 10; i >= -10; i--) {
+                System.out.println(i);
+
+            }
+        }
+
+        public static void task5() {
+            for (int i = 1984; i <= 2096; i = i + 4) {
+                System.out.println(i);
+
+            }
+        }
+
+        public static void task6() {
+            for (int i = 7; i <= 98; i = i + 7) {
+                System.out.println(i + " ");
+
+            }
+        }
+
+        public static void task7() {
+            for (int i = 1; i <= 512; i = i *2) {
+                System.out.println(i + " ");
+
+            }
+        }
+
+        public static void task8() {
+            int total = 0;
+            int salary = 29000;
+            for (int i = 0; i < 12; i++) {
+                total += salary;
+                System.out.println(total);
+            }
+        }
+
+        public static void task9() {
+            double total = 0;
+            double salary = 29000;
+            for (int i = 0; i < 12; i++) {
+                total = total * 1.01;
+                total += salary;
+                System.out.println(total);
+            }
+        }
+
+        public static void task10() {
+            for (int i = 1; i <= 10;  i++) {
+                System.out.println(" 2 * " + i + " = " + 2 * i);
+
+
+            }
+        }
+
     }
-}
+
